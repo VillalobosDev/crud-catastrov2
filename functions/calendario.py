5313a83cb66bf7747cb52d6aaa11ea84a544c577
Nelson <villalobosnelson000@gmail.com>
2025-01-23 15:29:11 -0400

    Si

--- functions/calendario.py
+++ functions/calendario.py
@@ -11,21 +11,22 @@ from config.config import centrar_ventana
 def open_calendar_popup(entry_widget):
     
     poppins12bold = ("Poppins", 12, "bold")
+    poppins14bold = ("Poppins", 14, "bold")
 
 
-    poppins8bold = ("Poppins", 8)
+    poppins8bold = ("Poppins", 10)
     
     """Open a calendar popup to select a date."""
     calendar_popup = ctk.CTkToplevel()
     calendar_popup.grab_set()
     calendar_popup.title("Fecha")
-    calendar_popup.geometry("320x310")
+    calendar_popup.geometry("390x310")
     calendar_popup.resizable(width=False, height=False)
     
-    centrar_ventana(calendar_popup, 320, 310)
+    centrar_ventana(calendar_popup, 340, 310)
     
     top_container = ctk.CTkFrame(calendar_popup, corner_radius=15)
-    top_container.pack(fill="x", padx=5, pady=5, side="top")
+    top_container.pack(fill="both", padx=5, pady=5, expand=True)
     
     frame_calendar=ctk.CTkFrame(top_container, corner_radius=15)
     frame_calendar.pack(pady=5, padx=5)
@@ -42,8 +43,9 @@ def open_calendar_popup(entry_widget):
         calendar_popup.destroy()  # Close the calendar popup
 
     # Add button to confirm date selection
-    select_button = ctk.CTkButton(top_container, text="Seleccionar", command=select_date, font=poppins12bold)
+    select_button = ctk.CTkButton(top_container, text="Seleccionar", command=select_date, font=poppins14bold)
     select_button.pack(pady=10, side="bottom")
+
 
 
 def create_date_range_selector(parent_frame, searchbtn, my_tree, original_data):
